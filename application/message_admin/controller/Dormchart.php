02785d1a5c5d34dd9fa07d549c90a2cb98f327ea
leimon <1285653363@qq.com>
2019-12-24 11:55:01 +0800

    寝室分配

--- application/message_admin/controller/Dormchart.php
+++ application/message_admin/controller/Dormchart.php
@@ -11,6 +11,7 @@ class Dormchart extends Base
 {
     protected $dorm_model;
     protected $student_model;
+    protected $dorm_log_model;
     /**
      * Dormchart constructor.
      */
@@ -19,6 +20,7 @@ class Dormchart extends Base
         parent::__construct();
         $this->dorm_model = model('Dorm');
         $this->student_model = model('Student');
+        $this->dorm_log_model = model('DormLog');
     }
 
     /**
@@ -29,7 +31,10 @@ class Dormchart extends Base
         $param = input('');
         $page = isset($param['page'])?(int)$param['page']:1;
         $limit = isset($param['limit'])?(int)$param['limit']:10;
-        
+        //年份
+        $y = isset($param['years'])?(int)$param['years']:date('Y');
+        //月份
+        $m = isset($param['months'])?(int)$param['months']:date('m');
         //获取所有床位
         $dorm_lists = $this->dorm_model->get_all_data_page(['status'=>1],$page,$limit,'','id,username');
         $data = [];
@@ -42,15 +47,18 @@ class Dormchart extends Base
              }
          }
         $data['years_arr'] = $new_year_arr;
-        //初始化为当前年份
-        $y=date('Y');
-        //初始化为当前月份
-        $m=date('m');
-        
+
+        //本月月初
+        $beginThismonth=mktime(0,0,0,$m,1,$y);
+        //本月月末
+        $endThismonth=mktime(23,59,59,$m,date('t'),$y);
+        //获取本月寝室占用情况
+        $dorm_log = $this->dorm_log_model->get_all_data(['inputtime'=>['lt',$endThismonth],'leavetime'=>['gt',$beginThismonth]],'','id,username,student_id,type,dorm_id,inputtime,leavetime',['student']);
         $data['years'] = $y;
         $data['months'] = $m;
         $data['days'] =  date("t",strtotime("$y-$m"));
         $data['dorm_lists'] =  $dorm_lists;
+        $data['dorm_log'] =  $dorm_log;
         return view('',$data);
     }
 
